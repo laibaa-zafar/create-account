8e88ee1e55d1e2c56f44ff57bf15fbc68ecae362
Laiba <laibazafar2699@gmail.com>
2024-05-17 00:56:13 +0500

    form

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById("signupModal");
+const btn = document.getElementById("signupBtn");
+const span = document.getElementById("closeBtn");
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
